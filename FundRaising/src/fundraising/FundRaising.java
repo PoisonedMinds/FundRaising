dfcf2c465fa969f329a703d6d86dc18ff0c2e539
PoisonedMinds <steven_biro@hotmail.com>
2015-02-25 16:06:27 -0500

    most of the work done

---
+++ FundRaising/src/fundraising/FundRaising.java
@@ -0,0 +1,71 @@
+
+
+package fundraising;
+import javax.swing.*;
+/**
+ * @title FundRaising
+ * @author Steven Biro
+ * @teacher Mr. J. Carron
+ * @date 25-Feb-2015 3:05:13 PM
+ * @purpose The purpose of this program is to
+ */
+public class FundRaising {
+
+    public static void main(String[] args) {
+        double central[]=new double[4],cross[]=new double[4],paul[]=new double[4],teresa[]=new double[4],mundi[]=new double[4],joseph[]=new double[4],mary[]=new double[4],thomas[]=new double[4];
+        int school = 0,test,amount = 0,pop;
+        double schools[][]={central,cross,paul,teresa,mundi,joseph,mary,thomas};//split declaration to make coding easier and more debugablem easier to see which school is which.
+        do {
+            test=0;
+        
+            try {
+            school=Integer.parseInt(JOptionPane.showInputDialog("Which school is doing fundraising?\n 0 - Catholic Central\n1 - Holy Cross\n2 - John Paul II\n3 - Mother Teresa\n4 - Regina Mundi\n5 - St.Joseph\n6 - St.Mary\n7 - St.Thomas Aquinas\n8 - EXIT"));
+            if (school==8) {
+                System.exit(1);
+            }
+        } catch(NumberFormatException e) {
+            test=1;
+        }
+        }  while (test==1);
+        do {
+            test=0;
+        
+            try {
+            amount=Integer.parseInt(JOptionPane.showInputDialog("What is the donation amount?\n0 - 25 cents\n1 - 50 cents\n2 - 1 dollar\n3 - 2 dollars\n4 - EXIT"));
+            if (amount==4) {
+                System.exit(2);
+            }
+        } catch(NumberFormatException e) {
+            test=1;
+        }
+        }  while (test==1);
+        pop=Integer.parseInt(JOptionPane.showInputDialog("What is the school population?"));
+        schools[school][amount]=pop*amountConvert(amount);
+        
+        
+        
+        
+    }
+    public static double amountConvert(int amount) {
+        double value = 0;
+        if (amount==0) {
+            value=0.25;
+        } else if (amount==1) {
+            value=0.5;
+        } else if (amount==2) {
+            value=1;
+        } else if (amount==3) {
+            value=2;
+        } else {
+            JOptionPane.showMessageDialog(null,"Invalid option for donation amount.");
+            System.exit(3);
+        }
+        return value;
+        
+        
+        
+        
+      
+        
+    }
+}
